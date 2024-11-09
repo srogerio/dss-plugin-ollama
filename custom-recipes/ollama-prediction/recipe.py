6fc6fda1ac17f329651a77ce82eeb7a30b3a9fed
Alexandre Bourret <alexandre.bourret@dataiku.com>
2024-11-09 19:18:25 +0100

    Add image input

--- custom-recipes/ollama-prediction/recipe.py
+++ custom-recipes/ollama-prediction/recipe.py
@@ -1,6 +1,8 @@
 import dataiku
 import pandas
 import requests
+import json
+import base64
 
 from dataiku.customrecipe import get_input_names_for_role
 from dataiku.customrecipe import get_output_names_for_role
@@ -8,39 +10,59 @@ from dataiku.customrecipe import get_recipe_config
 
 from ollama_common import (extract_string, process_template, get_configuration)
 
+
 input_A_names = get_input_names_for_role('input_A_role')
 input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
 
 output_A_names = get_output_names_for_role('main_output')
 config = get_recipe_config()
-
 prompts_df = input_A_datasets[0].get_dataframe()
 
-server_url, model_name = get_configuration(config)
+server_url, model_name, output_format = get_configuration(config)
 
 output = dataiku.Dataset(output_A_names[0])
 
 first_dataframe = True
 prompt_source = config.get("prompt_source", "column")
+prompt_column = config.get("prompt_column")
+image_column = config.get("image_column")
 with output.get_writer() as writer:
     unnested_items_rows = []
     for index, input_parameters_row in prompts_df.iterrows():
         row = input_parameters_row.to_dict()
         if prompt_source == "column":
-            prompt_template = row.get("prompt", "")
+            prompt_template = row.get(prompt_column, "")
         else:
             prompt_template = config.get("prompt", "")
         prompt = process_template(prompt_template, row)
+        image = None
+        if image_column:
+            image_url = row.get(image_column)
+            if image_url and image_url.startswith("http"):
+                # image = upload_image(image_url)
+                # image = base64.b64encode(image).decode('utf-8')
+                image = base64.b64encode(requests.get(image_url).content).decode('utf-8')
         data = {
           "model": "{}".format(model_name),
           "prompt": "{}".format(prompt)
          }
+        if image:
+            data["images"] = [image]
+        if output_format:
+            data["format"] = output_format
         try:
             response = requests.post(url="{}/api/generate".format(server_url), json=data)
         except Exception as error:
             raise Exception("Connection error: {}".format(error))
         answer = extract_string(response.content)
-        row['answer'] = answer
+        if output_format == 'json':
+            try:
+                row['answer'] = json.loads(answer)
+            except Exception as error:
+                print("Not a valid JSON", error)
+                row['answer'] = answer
+        else:
+            row['answer'] = answer
         unnested_items_rows.append(row)
     unnested_items_rows = pandas.DataFrame(unnested_items_rows)
     if first_dataframe:
--- python-lib/ollama_common.py
+++ python-lib/ollama_common.py
@@ -1,4 +1,8 @@
 import json
+# try:
+#     from BytesIO import BytesIO  # for Python 2
+# except ImportError:
+#     from io import BytesIO  # for Python 3
 
 
 def extract_string(source):
@@ -23,11 +27,106 @@ def process_template(template, dictionnary):
 
 
 def get_configuration(config):
+    output_format = None
     if config.get("display_advanced_parameters", False):
         server_url = config.get("server_url", "http://localhost:11434")
-        model_name = config.get("model_name", "mistral")
+        output_format = config.get("output_format", None)
     else:
         server_url = "http://localhost:11434"
-        model_name = "mistral"
+    model_name = config.get("model_select")
+    if not model_name:
+        model_name = config.get("model_name", "mistral")
     server_url = server_url.strip("/")
-    return server_url, model_name
+    return server_url, model_name, output_format
+
+
+MANUAL_SELECT_ENTRY = {"label": "✍️ Enter manually", "value": None}
+COLUMN_SELECT_ENTRY = {"label": "🏛️ Get from column", "value": "dku_column_select"}
+
+
+# def upload_image(image_url):
+#     import tempfile
+#     import requests
+#     output = None
+#     with tempfile.NamedTemporaryFile(delete=True) as tmp_file:
+#         response = requests.get(image_url)
+#         for chunk in response.iter_content(chunk_size=1024):
+#             if chunk:
+#                 tmp_file.write(chunk)
+#         with tempfile.NamedTemporaryFile(delete=True, suffix=".jpg") as tmp_jpg_file:
+#             tmp_jpg_file = jpeg_compress(tmp_file, tmp_jpg_file)
+#             tmp_jpg_file.seek(0)
+#             output = tmp_file.read()
+#     return output
+
+
+# def jpeg_compress(tmp_file, tmp_jpg_file):
+#     from PIL import Image
+#     tmp_file.flush()
+#     tmp_file.seek(0)
+#     raw_image = Image.open(tmp_file.name)
+#     buffer = BytesIO()
+#     raw_image.save(buffer, "JPEG", quality=70)
+#     tmp_jpg_file.write(buffer.getbuffer())
+#     tmp_jpg_file.flush()
+#     tmp_jpg_file.seek(0)
+#     return tmp_jpg_file
+
+
+class DSSSelectorChoices(object):
+
+    def __init__(self):
+        self.choices = []
+
+    def append(self, label, value):
+        self.choices.append(
+            {
+                "label": label,
+                "value": value
+            }
+        )
+
+    def append_alphabetically(self, new_label, new_value):
+        index = 0
+        new_choice = {
+            "label": new_label,
+            "value": new_value
+        }
+        for choice in self.choices:
+            choice_label = choice.get("label")
+            if choice_label < new_label:
+                index += 1
+            else:
+                break
+        self.choices.insert(index, new_choice)
+
+    def append_manual_select(self):
+        self.choices.append(MANUAL_SELECT_ENTRY)
+
+    def start_with_manual_select(self):
+        self.choices.insert(0, MANUAL_SELECT_ENTRY)
+
+    def append_column_select(self):
+        self.choices.append(COLUMN_SELECT_ENTRY)
+
+    def _build_select_choices(self, choices=None):
+        if not choices:
+            return {"choices": []}
+        if isinstance(choices, str):
+            return {"choices": [{"label": "{}".format(choices)}]}
+        if isinstance(choices, list):
+            return {"choices": choices}
+        if isinstance(choices, dict):
+            returned_choices = []
+            for choice_key in choices:
+                returned_choices.append({
+                    "label": choice_key,
+                    "value": choices.get(choice_key)
+                })
+            return returned_choices
+
+    def text_message(self, text_message):
+        return self._build_select_choices(text_message)
+
+    def to_dss(self):
+        return self._build_select_choices(self.choices)
